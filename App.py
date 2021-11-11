73888ec7bd1d94fd253192edc542644da7d6e71b
mvitta <mikesystem@hotmail.com>
2021-11-11 18:12:41 -0500

    paginaProductoIndividual

--- App.py
+++ App.py
@@ -1,18 +1,18 @@
 from logging import debug, error
 from sqlite3.dbapi2 import Cursor
-from flask import Flask, render_template, request, flash,session,redirect
+from flask import Flask, render_template, request, flash, session, redirect
 import os
 from flask import *
 import sqlite3
 from sqlite3 import Error
 import base64
-from markupsafe import escape 
+from markupsafe import escape
 import hashlib
 from werkzeug.exceptions import UnsupportedMediaType
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-ruta_index="index.html"
+ruta_index = "index.html"
 
 app = Flask(__name__)
 app.secret_key = os.urandom(34)
@@ -27,6 +27,10 @@ def login():
 def registro():
     return render_template('registro.html')
 
+
+@app.route('/producto', methods=['POST', 'GET'])
+def producto():
+    return render_template('baseProducto.html')
 
 
 if __name__ == "__main__":

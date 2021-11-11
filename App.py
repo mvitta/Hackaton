e01b9bd01edd43e8266513916d010b109c38e62c
mvitta <mikesystem@hotmail.com>
2021-11-11 07:35:17 -0500

    Inicio

---
+++ App.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(34)
+
+
+@app.route('/')
+def index():
+    return "Hola MUndo"
+
+
+def main():
+    if __name__ == "__main__":
+        app.run(debug=True)
+
+
+main()

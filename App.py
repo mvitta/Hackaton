7becf53eabfc99a1ea2eaec282df7a8d7d268eca
Osc2405 <orosero2405@gmail.com>
2021-11-11 10:29:08 -0500

    Creacion de BASE html y pagina de inicio

--- App.py
+++ App.py
@@ -1,5 +1,18 @@
-from flask import Flask
+from logging import debug, error
+from sqlite3.dbapi2 import Cursor
+from flask import Flask, render_template, request, flash,session,redirect
 import os
+from flask import *
+import sqlite3
+from sqlite3 import Error
+import base64
+from markupsafe import escape 
+import hashlib
+from werkzeug.exceptions import UnsupportedMediaType
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+ruta_index="index.html"
 
 app = Flask(__name__)
 app.secret_key = os.urandom(34)
@@ -7,12 +20,9 @@ app.secret_key = os.urandom(34)
 
 @app.route('/')
 def index():
-    return "Hola Tierra 7"
+    return render_template(ruta_index)
 
 
-def main():
-    if __name__ == "__main__":
-        app.run(debug=True)
 
-
-main()
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
